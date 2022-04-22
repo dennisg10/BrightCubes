d63b196523c9437282230b067818218a3513ad90
dennisg10 <dennis.glasbergen@outlook.com>
2022-04-22 11:05:38 +0200

    statefile

---
+++ terraform/provider.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 0.5"
+    }
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "sadgterraform001sf"
+    container_name       = "tfstate"
+    key                  = "backend.terraform.tfstate"
+    resource_group_name  = "rg-dgterraform"
+    use_azuread_auth     = false
+    subscription_id      = "274ed5a4-bc5f-4f7a-9b29-6bf02b73f2c8"
+
+  }
+}
+
+provider "sops" {
+}
+
